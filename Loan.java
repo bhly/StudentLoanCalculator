0d72996f5dbeee6d25d8522af7910c3081cd055b
Brynn Haley <brynn.haley@live.com>
2019-04-29 19:41:20 -0300

    Working on iterative approach to calculating lifetime interest accumulated on loan.

--- Loan.java
+++ Loan.java
@@ -1,63 +1,100 @@
+/**
+This program calculates the estimated monthly payment for a loan. 
+@author Brynn Haley
+*/
+
 import java.lang.Math;
 import java.text.DecimalFormat;
 
 public class Loan{
 
-	private double principal; //principle amount owing
-	private double interest; //interest rate 
-	private int term; //amount of years to pay off loan
+	/**
+	The original principal amount owing on the loan before interest is applied.
+	*/
+	private double principal; 
+	/** 
+	The annual interest rate of the loan.
+	*/
+	private double rate; 
+	/**
+	The amount of years it will take to pay off the loan.
+	*/
+	private int termYears; 
+	/**
+	The amount of months it will take to pay off the loan. Calculated as termYears*12
+	*/
+	private int termMonths;
+	/**
+	The estimated monthly payment for the loan.
+	*/
+	private double emi; 
+	/**
+	The monthly interest rate calculated on the loan.
+	*/
+	private double mr;
 	
 	DecimalFormat fmt = new DecimalFormat("0.##");
 	
-	public Loan(double principal, double interest, int term){
+	/**
+	Constructs a loan. Can be a student loan, mortgage, car loan, etc.
+	@param principal The amount owing on the loan before interest.
+	@param interest The annual interest rate of the loan. 
+	@param term The amount of months it will take to pay off the loan.
+	*/
+	public Loan(double principal, double rate, int termYears){
 		this.principal = principal;
-		this.interest = interest;
-		this.term = term;
+		this.rate = rate;
+		this.termYears = termYears;
 	}
 	
-	public double getPrincipal(){
-		return principal;
-	}
-	
-	public double getInterest(){
-		return interest;
-	}
-	
-	public int getTerm(){
-		return term;
-	}
-	
-	public void setPrincipal(double principal){
-		this.principal=principal;
-	}
-	
-	public void setInterest(double interest){
-		this.interest = interest;
-	}
-	
-	public void setTerm(int term){
-		this.term = term;
-	} 
-	
-	public String payment(double principal, double interest, int term){
-		double mi = interest / (12*100); //monthly interest
-		double emi = (principal * mi *(double) Math.pow(1 + mi, term)) / (double) (Math.pow(1 + mi, term) -1);
+	/** 
+	This method calculates the estimated monthly payment on the loan.
+	@param principal
+	@param rate
+	@param termYears 
+	*/
+	public String payment(double principal, double rate, int termYears){
+		termMonths = termYears*12;
+		mr = rate / (12*100); 
+		emi = (principal * mr *(double) Math.pow(1 + mr, termMonths)) / (double) (Math.pow(1 + mr, termMonths) -1);
 		return fmt.format(emi);
 	}
 	
-	//public double lifetimeInterest(double principal, double interest, double term){
-	//	return (
+	/**
+	This method calculates a month's worth of interest on the loan.
+	@param emi The monthly payment (generated from payment()).
+	@param principal
+	@param termYears
+	*/
+	public double monthlyInterest(double emi, double principal, int termYears){
+		termMonths = termYears*12;
+		return (rate / (12*100))*principal;
+	}
+		
+	/**This method calculates the total amount of interest that will accumulate over the lifetime of the loan.
+	@param emi
+	@param principal
+	@param termYears
+	*/
+	public double lifetimeInterest(double emi, double principal, int termYears){
+		termMonths = termYears/12;
+		double owing = 0;
+		for(int i = 0; i<termMonths-1;i++);
+			owing = principal + mr;
+			
+		termMonths = termYears*12;
+		
+	}
 
 	public static void main(String[] args){
 		
-		Loan loan = new Loan(40000, 3.5, 120);
+		Loan loan = new Loan(40000, 10, 120);
 			
-		System.out.println(loan.payment(loan.getPrincipal(), loan.getInterest(), loan.getTerm()));
+		System.out.println(loan.payment(loan.principal, loan.rate, loan.termYears));
+		System.out.println(loan.monthlyInterest(loan.emi, loan.principal, loan.termYears));
+		System.out.println(loan.lifetimeInterest(loan.emi, loan.principal, loan.termYears));
 	}
 	
 }
 
 
-
-//monthly payments 
-//lifetime interest 
